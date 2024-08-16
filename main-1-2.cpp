88f5708dcd41d2e74bf18bd9df3d4f5c256eed5d
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-08-16 11:42:14 +0930

    main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -3,21 +3,16 @@
 
 extern PersonList createPersonList(int n);
 
-int main()
-{
+int main() {
     int n = 5;
 
-    // use the function
     PersonList personList = createPersonList(n);
 
-    // output the name and age of every Person struct arrays
-    for (int i = 0; i < personList.numPeople; ++i)
-    {
+    for (int i = 0; i < personList.numPeople; ++i) {
         std::cout << "Person " << i + 1 << ": " << personList.people[i].name
                   << ", Age: " << personList.people[i].age << std::endl;
     }
 
-    // free the dynamically allocated memory
     delete[] personList.people;
 
     return 0;
